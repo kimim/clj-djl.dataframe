077cc0e6b74eab03b887f0805ca131366efff87e
kimim <kimi.im@outlook.com>
2020-12-14 03:23:11 +0000

    fix error in test cases

--- test/clj_djl/dataframe/missing_test.clj
+++ test/clj_djl/dataframe/missing_test.clj
@@ -62,13 +62,15 @@
     (is (= ["A" "A" "B" "B" "C" "C" "D"] (vec (ds' :d)))))
 
   (let [ds (ds/->dataframe {:a [nil 2 nil 4 nil 6 nil]
-                          :b [3 nil 6 nil 9 nil 12]
-                          :c [nil "A" nil "B" nil "C" nil]
-                          :d ["A" nil "B" nil "C" nil "D"]
-                          })
+                            :b [3 nil 6 nil 9 nil 12]
+                            :c [nil "A" nil "B" nil "C" nil]
+                            :d ["A" nil "B" nil "C" nil "D"]
+                            })
         ds' (ds/replace-missing ds :lerp)]
-    (is (= [2 2 3 4 5 6 6] (vec (ds' :a))))
-    (is (= [3 5 6 8 9 11 12] (vec (ds' :b))))
+    ;; replace missing will turn integer to float
+    ;; https://github.com/techascent/tech.ml.dataset/issues/184#issuecomment-743295024
+    (is (= [2.0 2.0 3.0 4.0 5.0 6.0 6.0] (vec (ds' :a))))
+    (is (= [3.0 4.5 6.0 7.5 9.0 10.5 12.0] (vec (ds' :b))))
     ;; :lerp for categorical, is same as :down
     (is (= ["A" "A" "A" "B" "B" "C" "C"] (vec (ds' :c))))
     (is (= ["A" "A" "B" "B" "C" "C" "D"] (vec (ds' :d)))))
